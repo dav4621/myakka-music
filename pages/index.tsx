23a3b567a7ccfb320262f6991d44d4c2dab4df19
john <jdav4621@gmail.com>
2023-02-12 22:23:06 -0500

    updated favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
 import Water from '@/styles/Water'
-
-import logo from '../public/logo.jpg'
 import TitleLink from '../components/TitleLink'
 
 export default function Home() {
